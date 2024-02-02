b8a8fa1b55243c88c782374e9dd7ff673354284f
mantvmass <kliop2317@gmail.com>
2024-02-03 04:32:28 +0700

    update README.md

--- pystratum/Stratum.py
+++ pystratum/Stratum.py
@@ -65,25 +65,3 @@ class Stratum:
                 nonce
             ]
         })
-
-
-def handleReceive(stratum: Stratum, response: Dict[str, any]) -> None:
-    print(response)
-    id = response.get("id", 0)
-    if id == None:
-        pass
-    elif id == 1:
-        stratum.authorize()
-    
-        
-
-def main():
-    stratum_client = Stratum("ap.luckpool.net", 3956, "RQpWNdNZ4LQ5yHUM3VAVuhUmMMiMuGLUhT.pystratum", "x")
-    stratum_client.connect()
-    stratum_client.subscribe()
-    stratum_client.onReceive(stratum_client, handleReceive)
-   
-        
-
-if __name__ == "__main__":
-    main()
