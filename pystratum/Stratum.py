974da01bb625e95a2c458c044a7c8d576c3b47c9
mantvmass <kliop2317@gmail.com>
2024-02-02 21:11:36 +0700

    fix receive message error

--- pystratum/Stratum.py
+++ pystratum/Stratum.py
@@ -2,6 +2,7 @@ import socket
 import json
 import asyncio
 from typing import Union
+import time
 
 
 class Stratum:
@@ -20,15 +21,26 @@ class Stratum:
         self.__username = username
         self.__password = password
         self.__sock = None
-        self.is_connected: False
+        self.is_connected = False
     
     
     async def send(self, request: dict) -> bool:
         self.__sock.sendall((json.dumps(request) + "\n").encode("utf-8"))
 
 
-    async def receive(self) -> Union[dict, bytes]:
-        return json.loads(self.__sock.recv(1024).decode("utf-8"))
+    # async def receive(self) -> Union[dict, bytes]:
+    async def receive(self) -> dict:
+        print("==================================================================================")
+        
+        # json_objects = [json.loads(obj) for obj in self.__sock.recv(1024).decode("utf-8").split('\n') if obj.strip()]
+        
+        json_objects = json.loads(self.__sock.recv(1024).decode("utf-8"))
+    
+        # json_objects = self.__sock.recv(1024)
+        
+        print(json_objects)
+        return json_objects
+
         
 
     async def connect(self):
@@ -60,15 +72,50 @@ class Stratum:
     
         
 
-# Example usage:
 async def main():
-    stratum_client = Stratum("sg.ss.btc.com", 1800, "gf", "fg")
-    await stratum_client.connect()
-    await stratum_client.subscribe()
-    print(await stratum_client.receive())
-    await stratum_client.authorize()
-    print(await stratum_client.receive())
+    # stratum_client = Stratum("sg.ss.btc.com", 1800, "gf", "fg")
+    # await stratum_client.connect()
+    
+    # await stratum_client.subscribe()
+    
+    # while True:
+        
+    #     result = await stratum_client.receive()
+        
+    #     print(result)
+
+    #     if result["id"] == 1:
+    #         await stratum_client.authorize()
+        
+    #     time.sleep(2)
+    
+    from pprint import pprint
+    
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(("ap.luckpool.net", 3956))
+    client_socket.sendall((json.dumps({"id": 1, "method": "mining.subscribe", "params": []}) + "\n").encode("utf-8"))
+    print(client_socket.recv(1028).decode("utf-8"))
+    client_socket.sendall((json.dumps({"id": 2, "method": "mining.authorize", "params": ["dsfgd", "x"]}) + "\n").encode("utf-8"))
+    print(client_socket.recv(1028).decode("utf-8"))
+    while 1:
+        
+        # pprint(json.loads(client_socket.recv(1028).decode("utf-8")))
+        # print(client_socket.recv(1028).decode("utf-8"))
+        
+        
+        data = client_socket.recv(1028).decode("utf-8")
+        
+        
+        # แยก JSON strings
+        json_strings = [chunk for chunk in data.split('\n') if chunk]
+        
+        for i in json_strings:
+            pprint(json.loads(i))
+            print("===================================================================================")
+    
+   
+        
 
 if __name__ == "__main__":
     asyncio.run(main())
-    
+    pass
